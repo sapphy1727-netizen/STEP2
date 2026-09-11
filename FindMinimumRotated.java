099c5456adba2f4fce045ed388c58558492324ef
sapphy1727-netizen <sapphy1727@gmail.com>
2026-09-11 22:37:49 +0530

    WEEK4HW5

---
+++ FindMinimumRotated.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class FindMinimumRotated {
+
+    static int findMin(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low < high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] > nums[high]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+
+        return nums[low];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int result = findMin(nums);
+
+        System.out.println("Minimum Element: " + result);
+
+        sc.close();
+    }
+}
