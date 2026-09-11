a636b7e7f9148ae93946a700ab4d9f30f80a14e4
sapphy1727-netizen <sapphy1727@gmail.com>
2026-09-11 23:02:17 +0530

    cw1

---
+++ WEEK5Q1.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class WEEK5Q1 {
+
+    static void curveScores(int[] scores, int bonus) {
+        for (int i = 0; i < scores.length; i++) {
+            scores[i] = scores[i] + bonus;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of teams: ");
+        int n = sc.nextInt();
+
+        int[] scores = new int[n];
+
+        System.out.println("Enter team scores:");
+        for (int i = 0; i < n; i++) {
+            scores[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter bonus: ");
+        int bonus = sc.nextInt();
+
+        curveScores(scores, bonus);
+
+        System.out.println("Updated Scores: " + Arrays.toString(scores));
+
+        sc.close();
+    }
+}
