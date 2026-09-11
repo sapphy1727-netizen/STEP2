dadee1b16128f4e54ce49591e86cb0ff59680a09
sapphy1727-netizen <sapphy1727@gmail.com>
2026-09-11 22:23:29 +0530

    WEEK4HW3

---
+++ ThreeSum.java
@@ -0,0 +1,83 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class ThreeSum {
+
+    static int[][] threeSum(int[] nums) {
+        Arrays.sort(nums);
+
+        ArrayList<int[]> list = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    list.add(new int[]{nums[i], nums[left], nums[right]});
+
+                    int leftValue = nums[left];
+                    int rightValue = nums[right];
+
+                    while (left < right && nums[left] == leftValue) {
+                        left++;
+                    }
+
+                    while (left < right && nums[right] == rightValue) {
+                        right--;
+                    }
+
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        int[][] result = new int[list.size()][3];
+
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int[][] result = threeSum(nums);
+
+        System.out.println("Triplets:");
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(
+                "[" + result[i][0] + ", " +
+                result[i][1] + ", " +
+                result[i][2] + "]"
+            );
+        }
+
+        sc.close();
+    }
+}
